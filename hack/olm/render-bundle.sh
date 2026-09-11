cef65265f04e3f3995aa17487306e88655ed121d
Tullio Sebastiani <tsebasti@redhat.com>
2026-09-11 16:09:11 +0200

    fix(olm): align minimum Kubernetes version
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- hack/olm/render-bundle.sh
+++ hack/olm/render-bundle.sh
@@ -54,7 +54,7 @@ command -v yq >/dev/null || { echo "yq is required" >&2; exit 1; }
 operator_image=${OPERATOR_IMAGE:-krkn-chaos.docker.scarf.sh/krkn-chaos/krkn-operator:${version}}
 data_provider_image=${DATA_PROVIDER_IMAGE:-krkn-chaos.docker.scarf.sh/krkn-chaos/krkn-operator-data-provider:${version}}
 console_image=${CONSOLE_IMAGE:-krkn-chaos.docker.scarf.sh/krkn-chaos/krkn-operator-console:latest}
-min_kube_version=${MIN_KUBE_VERSION:-1.36.0}
+min_kube_version=${MIN_KUBE_VERSION:-1.19.0}
 openshift_versions=${OPENSHIFT_VERSIONS:-v4.19-v4.20}
 export OPERATOR_IMAGE="$operator_image"
 export DATA_PROVIDER_IMAGE="$data_provider_image"
